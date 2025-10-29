ce6a1b16f5d982fed9a8041a49b6ee9354031d9c
mindChickey <w453908766@126.com>
2025-10-29 22:03:32 +0800

    add alias

--- reg_bashrc/reg_bashrc.sh
+++ reg_bashrc/reg_bashrc.sh
@@ -19,3 +19,12 @@ push_path(){
   echo -e "export PATH=\$PATH:$(pwd)" >> ~/.bashrc
   source ~/.bashrc
 }
+
+alias pss='ps aux | grep '
+alias tss='ss -tulpn | grep '
+
+alias ssh1='ssh -L 3111:localhost:3111 server'
+
+
+alias clang1='clang -O -S -emit-llvm -fno-inline -fno-exceptions'
+alias clang++1='clang1 -O -S -emit-llvm -fno-inline -fno-exceptions'
