b664b5e50476f47a4ee90b66241383073ce2ee20
mindChickey <w453908766@126.com>
2026-01-28 18:09:42 +0800

    update

--- reg_bashrc/reg_bashrc.sh
+++ reg_bashrc/reg_bashrc.sh
@@ -10,8 +10,9 @@ CD()
 }
 alias cd='CD'
 
-alias rm0=rm
-alias rm=trash
+alias rm0='/bin/rm'
+alias rmm='trash -rf'
+alias rm="echo Warning: Use \'rm0\' for real delete or \'rmm\' for trash."
 
 set -o vi
 
@@ -23,8 +24,7 @@ push_path(){
 alias pss='ps aux | grep '
 alias tss='ss -tulpn | grep '
 
-alias ssh1='ssh -L 3111:localhost:3111 server'
-
+alias ssh1='ssh -L 3111:localhost:3111  -L 27018:localhost:27017 server'
 
 alias clang1='clang -O -S -emit-llvm -fno-inline -fno-exceptions'
 alias clang++1='clang1 -O -S -emit-llvm -fno-inline -fno-exceptions'
--- reg_bashrc/reg_gitpush.sh
+++ reg_bashrc/reg_gitpush.sh
@@ -22,13 +22,15 @@ gitpush() {
 }
 
 gitlines() {
+    local total_lines=0  # 定义累加变量
     for repo in "$@"; do
         if [ -d "$repo/.git" ]; then
-	    lines=$( (cd "$repo" > /dev/null && git ls-files -z | xargs -0 wc -l) 2>/dev/null | tail -n 1 | awk '{print $1}' )
+            lines=$( (cd "$repo" > /dev/null && git ls-files -z | xargs -0 wc -l) 2>/dev/null | tail -n 1 | awk '{print $1}' )
             count=${lines:-0}
+            total_lines=$((total_lines + count))
             printf "%-25s : %s lines\n" "$repo" "$count"
-        else
-            echo "跳过: $repo (不是有效的 Git 仓库)"
         fi
     done
+    echo "------------------------------------------------"
+    printf "%-25s : %s lines\n" "TOTAL" "$total_lines"
 }
