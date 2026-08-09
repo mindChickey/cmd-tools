21d2d750734c552a41c0831a865311f9b9ee2479
mindChickey <w453908766@126.com>
2026-08-09 11:26:47 +0800

    add bunwatch

--- reg_bashrc.sh
+++ reg_bashrc.sh
@@ -3,7 +3,7 @@ HISTSIZE=100000
 HISTFILESIZE=200000
 HISTTIMEFORMAT="%F %T "
 
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\][\t]\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\]\$ '
+PS1='${debian_chroot:+($debian_chroot)}\[\e]0;\w\a\]\[\033[01;32m\][\t]\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\]\$ '
 
 vd(){
   builtin cd "${1:-$HOME}"
@@ -38,3 +38,5 @@ clang-asm() {
 }
 
 alias oclip='xclip -selection clipboard -o'
+
+alias bunwatch='bun build --outdir . --watch'
--- reg_fcopy.sh
+++ reg_fcopy.sh
@@ -9,11 +9,21 @@ fcopy() {
   URI_LIST=""
   
   for file_path in "$@"; do
+    if [ ! -e "$file_path" ]; then
+      echo "错误: 路径不存在 -> $file_path" >&2
+      continue
+    fi
+
     ABSOLUTE_PATH=$(readlink -f "$file_path")
     FILE_URI="file://$ABSOLUTE_PATH"
     URI_LIST+="$FILE_URI"$'\n'
   done
   
+  if [ -z "$URI_LIST" ]; then
+    echo "没有有效的路径被复制。" >&2
+    return 1
+  fi
+
   echo -n "$URI_LIST" | xclip -selection clipboard -t text/uri-list
   
   echo "文件URI已复制到剪贴板:"
